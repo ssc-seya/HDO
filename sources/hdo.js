6abe08acb1414ee9dd699e47a31f457232c2b25c
Kisigi <kisigi.live@gmail.com>
2021-06-23 18:14:37 +0700

    basic code structure

---
+++ sources/hdo.js
@@ -0,0 +1,57 @@
+/** init */
+window.HDO = {
+    "code": "HDO",
+    "name": "HTML Data Object",
+    "format": {
+        "t": "tag",
+        "tag": "tag",
+        "tagName": "tag",
+        "e": "tag",
+        "element": "tag",
+        "a": "attributes",
+        "attr": "attributes",
+        "attributes": "attributes",
+        "b": "children",
+        "body": "children",
+        "c": "children",
+        "children": "children"
+    }
+};
+
+/** */
+HDO.NodeObject = function(data) {
+    
+    /* Create HTMLElement */
+    let element = HDO.HTML("div");
+
+    /* */
+    return element;
+
+};
+
+/** */
+HDO.NodeArray = function(data) {
+    
+    /* */
+    let list = [];
+
+    /* */
+    return list;
+
+};
+
+/** */
+HDO.HTML = function(tag, attributes = {}, children = []) {
+
+    /* */
+    let element = document.createElement(tag);
+
+    /* */
+    return element;
+
+};
+
+/** */
+HDO.Text = function(text) {
+    return document.createTextNode(text);
+};
